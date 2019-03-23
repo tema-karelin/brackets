81b4316cdbdf05aee033390e82a7be630b45568c
Artem Karelin <temi41995@gmail.com>
2019-03-23 22:05:52 +0300

    fix: correct function

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ module.exports = function check(str, bracketsConfig) {
   let bracket=[];
   for ( let i = 0; i < str.length; i++ ) {
     for (let n=0; n < bracketsConfig.length; n++) {
-      if ( str[i] == bracketsConfig[0][0]) {bracket.push(str[i]);} else {
-        if (bracket.length == 0) {return false};
-        if (str[i] == bracketsConfig[0][1] && bracket[bracket.length - 1] == bracketsConfig[0][0]) {bracket.pop();};
+      if (str[i] == bracketsConfig[n][0] && str[i] != bracket[bracket.length - 1] || str[i] == bracketsConfig[n][0] && bracketsConfig[n][0] != bracketsConfig[n][1]) {bracket.push(str.charAt(i));} else {
+        if (bracket.length == 0 && str[i] == bracketsConfig[n][1]) {return false};
+        if ((str[i] == bracketsConfig[n][1]) && (bracket[bracket.length - 1] == bracketsConfig[n][0])) {bracket.pop();};
         };
     };
           
